50d16a9f504ddbe6df6353e138685016a5656048
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-09-13 23:41:01 -0700

    working on weather js and adding api ish

--- js/weather.js
+++ js/weather.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
 	$('#submitWeather').click(function(){
 		
-		var city = $("city").val();
+		var api = "http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=1cd6a8f45fd1f15a776c9e14febf90ce";
 		
-		if(city != ' '){
-			
-		{else}
-			$("#error).html('Field cannot be empty');
-		}
+		$.getJSON(api. function(data){
+			alert(data.coord.lat);
+		
+		
+		// show the local weather project
+
 	});
 	
 });
