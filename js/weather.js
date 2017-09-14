0ff368e3dd11a059f842cb6f531cf81f6638197d
Roslyn Harris <lilmsrosie87@yahoo.com>
2017-09-13 23:15:24 -0700

    adding javascript to weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,13 @@
 $(document).ready(function(){
 	$('#submitWeather').click(function(){
 		
+		var city = $("city").val();
+		
+		if(city != ' '){
+			
+		{else}
+			$("#error).html('Field cannot be empty');
+		}
 	});
 	
 });
